7c2399e0f62e940398e1172c3f00754822008c33
Vanesa Bordanaro <112989077+VanesaBordanaro@users.noreply.github.com>
2023-09-29 13:32:39 -0300

    First commit

---
+++ scipt.js
@@ -0,0 +1,29 @@
+let myTasks = []
+
+const inputTask = document.getElementById("add-task")
+const inputBtn = document.getElementById("task-btn")
+const ulEl = document.getElementById("checkbox")
+const taskFromLocalStorage = JSON.parse(localStorage.getItem("myTasks"))
+
+if(taskFromLocalStorage) {
+    myTasks = taskFromLocalStorage
+    render(myTasks)
+}
+
+function render(task) {
+    let listItems = ""
+    for(let i = 0; i < task.length; i++) {
+        listItems += `
+            <li>
+                ${task[i]}
+            </li>`
+    }
+    ulEl.innerHTML = listItems
+}
+
+inputBtn.addEventListener('click', function(){
+    myTasks.push(inputTask.value)
+    inputTask.value = ""
+    localStorage.setItem("tasks", JSON.stringify(myTasks))
+    render(myTasks)
+})
