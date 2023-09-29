bb6d041f7889a5160e556a0dc3f0d01877dada9f
Vanesa Bordanaro <112989077+VanesaBordanaro@users.noreply.github.com>
2023-09-29 14:29:21 -0300

    Add refresh button functionality

--- scipt.js
+++ scipt.js
@@ -2,6 +2,7 @@ let myTasks = []
 
 const inputTask = document.getElementById("add-task")
 const inputBtn = document.getElementById("task-btn")
+const refreshBtn = document.getElementById("refresh-btn")
 const ulEl = document.getElementById("checkbox")
 const taskFromLocalStorage = JSON.parse(localStorage.getItem("myTasks"))
 
@@ -25,5 +26,11 @@ inputBtn.addEventListener('click', function(){
     myTasks.push(inputTask.value)
     inputTask.value = ""
     localStorage.setItem("tasks", JSON.stringify(myTasks))
+    render(myTasks)
+})
+
+refreshBtn.addEventListener('click', function() {
+    localStorage.clear()
+    myTasks = []
     render(myTasks)
 })
