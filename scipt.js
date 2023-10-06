cdf04d393152afd9f6814eb91a4c4680e7180d48
Vanesa Bordanaro <112989077+VanesaBordanaro@users.noreply.github.com>
2023-10-05 22:58:42 -0300

    Style li elements

--- scipt.js
+++ scipt.js
@@ -1,62 +1,62 @@
-let tasks = [];
+let tasks = []
 
-const inputTask = document.getElementById("add-task");
-const taskBtn = document.getElementById("task-btn");
-const ulEl = document.getElementById("ul-el");
-const refreshBtn = document.getElementById("refresh-btn");
-const taskFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+const inputTask = document.getElementById("add-task")
+const taskBtn = document.getElementById("task-btn")
+const ulEl = document.getElementById("ul-el")
+const refreshBtn = document.getElementById("refresh-btn")
+const taskFromLocalStorage = JSON.parse(localStorage.getItem("tasks"))
 
 if (taskFromLocalStorage) {
-  tasks = taskFromLocalStorage;
-  render(tasks);
+  tasks = taskFromLocalStorage
+  render(tasks)
 }
 
 function render(taskList) {
-  ulEl.innerHTML = "";
+  ulEl.innerHTML = ""
   taskList.forEach((task, index) => {
-    const li = document.createElement("li");
+    const li = document.createElement("li")
     li.innerHTML = `
       ${task}
-      <div id="tasks-bnts"
+      <span id="tasks-btns">
       <button class="delete-btn" onclick="deleteTask(${index})">
         <img src="trash1.png" alt="Delete">
       </button>
       <button class="complete-btn" onclick="completeTask(${index})">
         <img src="check.png" alt="Complete">
       </button>
-      </div>
+      </span>
     `;
-    ulEl.appendChild(li);
+    ulEl.appendChild(li)
   });
-  updateLocalStorage();
+  updateLocalStorage()
 }
 
 function deleteTask(index) {
-  tasks.splice(index, 1);
-  render(tasks);
+  tasks.splice(index, 1)
+  render(tasks)
 }
 
 function completeTask(index) {
-  const li = ulEl.children[index];
-  li.style.textDecoration = "line-through";
+  const li = ulEl.children[index]
+  li.style.textDecoration = "line-through"
 }
 
 function updateLocalStorage() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks))
 }
 
 taskBtn.addEventListener("click", function () {
-  const newTask = inputTask.value.trim();
+  const newTask = inputTask.value.trim()
   if (newTask !== "") {
-    tasks.push(newTask);
-    inputTask.value = "";
-    render(tasks);
+    tasks.push(newTask)
+    inputTask.value = ""
+    render(tasks)
   }
 });
 
 refreshBtn.addEventListener("click", function () {
-  localStorage.clear();
-  tasks = [];
-  render(tasks);
-});
+  localStorage.clear()
+  tasks = []
+  render(tasks)
+})
 
